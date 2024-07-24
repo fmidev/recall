21084cf68e0bd2543742cf77da9f5d02e73bdf13
Jussi Tiira <jussi.tiira@fmi.fi>
2024-07-24 17:00:53 +0300

    terracotta insert

---
+++ src/prevent/terradarcotta.py
@@ -0,0 +1,59 @@
+"""Ingest GeoTIFF files from S3 into a terracotta database."""
+
+import os
+import re
+import datetime
+
+import rasterio
+from rasterio.session import AWSSession
+from rasterio.errors import CRSError
+from botocore import UNSIGNED
+from botocore.config import Config
+import boto3
+import terracotta as tc
+from terracotta.exceptions import InvalidDatabaseError
+
+S3_BUCKET = 'fmi-opendata-radar-geotiff'
+KEYS = ('timestamp', 'radar', 'product')
+KEY_DESCRIPTIONS = {
+    'timestamp': 'Measurement timestamp',
+    'radar': 'Radar site',
+    'product': 'Product type'
+}
+DB_URI = 'postgresql://preventuser:kukkakaalisinappi@localhost/terracotta'
+
+driver = tc.get_driver(DB_URI)
+config = Config(signature_version=UNSIGNED, region_name='eu-west-1')
+s3 = boto3.resource('s3', config=config)
+bucket = s3.Bucket(S3_BUCKET)
+
+# sanity 
+try:
+    assert driver.key_names == KEYS
+except InvalidDatabaseError:
+    driver.create(KEYS, key_descriptions=KEY_DESCRIPTIONS)
+    assert driver.key_names == KEYS
+
+available_datasets = driver.get_datasets()
+
+
+def get_s3path(timestamp: datetime.datetime, radar: str, product: str):
+    return f's3://{S3_BUCKET}/{timestamp.strftime("%Y/%m/%d")}/{radar}/{timestamp.strftime("%Y%m%d%H%M")}_{radar}_{product}.tif'
+
+
+def insert(timestamp: datetime.datetime, radar: str, product: str):
+    """Insert radar metadata into the terracotta database."""
+    s3path = get_s3path(timestamp, radar, product)
+    tstr = timestamp.strftime('%Y%m%d%H%M')
+    keys = (tstr, radar, product)
+    if keys in available_datasets:
+        print('Skipping', s3path)
+        return
+    print('Ingesting', s3path)
+    with driver.connect():
+        with rasterio.Env(AWSSession(boto3.Session(), requester_pays=False), AWS_NO_SIGN_REQUEST='YES'):
+            try:
+                driver.insert(keys, s3path)
+            except CRSError:
+                print('CRSError, skipping')
+
