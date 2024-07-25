a06f89f94597a1e874049c7f9362b6407f3d89ca
Jussi Tiira <jussi.tiira@fmi.fi>
2024-07-25 15:58:43 +0300

    rm files of the flask version

--- src/prevent/forms.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SelectField
-from wtforms.validators import DataRequired
-
-class EventSelectionForm(FlaskForm):
-    event = SelectField('Select Event', validators=[DataRequired()], coerce=int)
