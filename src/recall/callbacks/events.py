2c2b4f43d1f376de63646f369ecac1191cd21b6c
Jussi Tiira <jussi.tiira@fmi.fi>
2024-09-02 16:45:38 +0300

    disable add-event button if required fields are empty

--- src/recall/callbacks/events.py
+++ src/recall/callbacks/events.py
@@ -10,3 +10,14 @@ from dash import Input, Output, callback
 def update_end_time_min(start_time):
     """Update the minimum value of the end time input."""
     return start_time
+
+
+@callback(
+    Output('add-event', 'disabled'),
+    Input('start-time', 'value'),
+    Input('end-time', 'value'),
+    Input('radar-picker', 'value'),
+)
+def disable_add_event_button(start_time, end_time, radar_id):
+    """Disable the add-event button if required fields are empty."""
+    return not all([start_time, end_time, radar_id])
